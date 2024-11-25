f280ff79eac308b87f95b00348c8701108f342cc
amaan@gmail.com <amaan@gmail.com>
2024-11-25 14:43:50 +0530

    Created a file using fs module with async/await

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const fs = require('fs').promises;
+
 const myPromise = new Promise((resolve, reject) => {
     const message = "Hii, This is Amaan";
     resolve(message); // Resolving the promise with a message
@@ -10,3 +12,15 @@ const myPromise = new Promise((resolve, reject) => {
     console.log(error);
   });
   
+  async function writeToFile() {
+    const message = "This is a message written to the file using async/await!";
+  
+    try {
+      // Writing the message to message.txt file
+      await fs.writeFile('message.txt', message);
+      console.log('Message written to file successfully!');
+    } catch (error) {
+      console.log('Error writing to file:', error);
+    }
+  }
+  writeToFile();  
